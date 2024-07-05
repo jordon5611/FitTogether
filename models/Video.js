d65606fc50d2ff3311fcb518931cbe9ea4aa4a47
Bhatti <jordon5611@gmail.com>
2024-07-05 23:00:30 +0500

    Subbscriptioon ADDED

--- models/Video.js
+++ models/Video.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const VideoSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    title: { type: String, required: true },
+    title: { type: String },
     videoUrl: { type: String, required: true },
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
