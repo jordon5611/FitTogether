35da8dcb06464023d56f20471d084e30b236c590
Bhatti <jordon5611@gmail.com>
2024-07-06 08:08:43 +0500

    Subbscriptioon ADDED

--- routes/trainer.js
+++ routes/trainer.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const Chat = require('../models/Chat');
+
+const Subscription = require('../models/Subscription');
 const { NotFoundError, BadRequestError } = require('../errors');
 
 //Middlewares
